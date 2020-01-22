86e300d30bde11836f6e32990ead58f40909cd84
Samar Omar <samaromm98@gmail.com>
2020-01-22 22:39:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
