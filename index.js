031906f194b65945c58f787b7a7832d48ba8ca3d
Samar Omar <samaromm98@gmail.com>
2020-01-22 22:45:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   let modi= document.getElementById('text');
-  modi.innerHTML='This is relly cool!';
+  modi.innerHTML='This is really cool!';
   
 });
 
